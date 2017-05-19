16453f6e1067f3303658cc61933b38d1ead78ca5
Kurt Neaderhiser <kurt.neaderhiser@gmail.com>
2017-05-19 15:08:00 -0400

    checkpoint 23 completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,7 @@
 class Topic < ApplicationRecord
    has_many :posts, :sponsored_posts, dependent: :destroy
+
+         validates :title, length: { minimum: 5 }, presence: true
+   validates :description, length: { minimum: 15 }, presence: true
+
 end
