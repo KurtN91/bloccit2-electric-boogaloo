f5da9902f93de1293ef99c09bf2854b2cebec431
Kurt Neaderhiser <kurt.neaderhiser@gmail.com>
2017-05-31 16:55:07 -0400

    fix attempt 1

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < ApplicationRecord
-   has_many :posts, :sponsored_posts, dependent: :destroy
+   has_many :posts
+   has_many :sponsored_posts, dependent: :destroy
 
          validates :title, length: { minimum: 5 }, presence: true
    validates :description, length: { minimum: 15 }, presence: true
