fedf6f42089270941becf5c8fbaf9d950bcedc44
Kurt Neaderhiser <kurt.neaderhiser@gmail.com>
2017-06-05 16:23:56 -0400

    checkpoint 30 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,13 +22,16 @@
  # Create Posts
  50.times do
  # #1
-   Post.create!(
+   post = Post.create!(
  # #2
       user:   users.sample,
       topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
    )
+   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+ # #13
+   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
  end
  posts = Post.all
  
@@ -70,6 +73,7 @@ Post.find_or_create_by!(title: "This is unique", body: "this is also unique")
   puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+  puts "#{Vote.count} votes created"
 
  # Create Sponsored Posts
  50.times do
